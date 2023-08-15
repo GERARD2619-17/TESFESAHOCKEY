59c16a5f791de5ea186f171a4f2f79d74f251349
GERARD2619-17 <gera121134@gmail.com>
2023-08-14 23:10:20 -0600

    REMEBER

--- Service_Worker.js
+++ Service_Worker.js
@@ -1,6 +1,6 @@
 
 //asignar un nombre y versión al cache
-const CACHE_NAME = 'WebDeveloper',
+const CACHE_NAME = 'FESAHOCKEYWEB',
   urlsToCache = [
     './',
     './index.html',
@@ -63,17 +63,38 @@ self.addEventListener('install', e => {
 })
 
 //una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
-self.addEventListener("activate", (e) =>{
-  console.log('se activo')
-})
-//almacenamiento cahe :D
-self.addEventListener("fetch", (e) => {
-console.log(`Intercepting fetch request for: ${e.request.url}`);
-e.respondWith(
-  caches.match(
-    e.request
-    
-  ).then(respuesta =>  respuesta)
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME]
 
-);
-});
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
+
+//cuando el navegador recupera una url
+self.addEventListener('fetch', e => {
+  //Responder ya sea con el objeto en caché o continuar y buscar la url real
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+          //recuperar del cache
+          return res
+        }
+        //recuperar de la petición a la url
+        return fetch(e.request)
+      })
+  )
+})
